c949c1321d210f5ba7d79ea335c5c65910d538a9
HoChiTai <hochitai24@gmail.com>
2022-02-26 22:05:55 +0700

    add source code

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 80)),
+              const Text('hello'),
+              const Padding(padding: EdgeInsets.only(top: 80)),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 40)),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello!')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
